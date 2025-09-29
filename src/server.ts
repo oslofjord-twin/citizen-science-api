997c8fc68179159db2e7a735b28f254802470112
Marius Holten <mariuseholten@gmail.com>
2025-09-29 20:13:50 +0200

    remove unnecessary cors configurations

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Better-Auth-Token']
+  allowedHeaders: ['Content-Type', 'Cookie']
 }));
 
 // Better Auth routes
