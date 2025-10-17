b990c144c1de667df01a14b2d20c5411baf7c947
Marius Holten <mariuseholten@gmail.com>
2025-10-17 11:24:33 +0200

    remove leaderboardRoute

--- src/routes/leaderboard.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authenticateUser } from "../middleware/auth.js";
-
-const router = Router();
-
-router.get("/leaderboard", authenticateUser, measurementController.getMeasurements);
-
-export default router;
