2736daaa86dd7b17e4a62153bc2997b2048e006f
Marius Holten <mariuseholten@gmail.com>
2025-10-17 11:50:54 +0200

    remove customSession plugin

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,6 @@ import { Pool } from "pg";
 import { betterAuth } from "better-auth";
 import { openAPI } from "better-auth/plugins";
 import { expo } from "@better-auth/expo";
-import { customSession } from "better-auth/plugins";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -21,16 +20,6 @@ export const auth = betterAuth({
   plugins: [
     expo(),
     openAPI(),
-    customSession(async ({ user, session }) => {
-      return {
-        user: {
-          ...user,
-          level: user.level,
-          total_points: user.total_points,
-        },
-        session,
-      };
-    }),
   ],
 
   user: {
