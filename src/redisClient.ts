3c8bf0746b7c7d69f165b3e5d6ccd88be1363ebb
Marius Holten <mariuseholten@gmail.com>
2025-10-20 15:28:14 +0200

    add url

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 redis.on("connect", () => console.log("✅ Connected to Redis"));
 redis.on("error", (err) => console.error("❌ Redis error:", err));
