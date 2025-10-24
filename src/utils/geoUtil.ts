a45d5590e2c22a7048bb32cbb1b1d410dfe6abc0
Marius Holten <mariuseholten@gmail.com>
2025-10-24 15:33:51 +0200

    update import

--- src/utils/geoUtil.ts
+++ src/utils/geoUtil.ts
@@ -1,4 +1,4 @@
-import area from "../data/oslofjordArea.json"
+import area from "../data/oslofjordArea.json" assert { type: "json" };
 
 type Coordinate = [number, number];
 type Ring = Coordinate[];
